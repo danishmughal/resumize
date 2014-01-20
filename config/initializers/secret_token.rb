5e131cf7077ca0a8f847d80e735a0eb2516a7a21
Danish Mughal <mughal.danish@gmail.com>
2014-01-20 08:56:09 -0600

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resumize::Application.config.secret_key_base = '7954b3bf5ced79ddfd6e1eb375ded53765f78476d8cdade59478e6ffc23bae120890c39b029dbc1271b27907cb397fda0faaa9fc99a7a063f9b089b8d2b92242'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Resumize::Application.config.session_store :cookie_store, key: '_resumize_session'
